2d47d0fef250efd18e3dde46a76738f47741736d
soap-cpp <995560375@qq.com>
2022-11-07 20:51:13 -0800

    complete server

--- server/src/ServerInfo.ts
+++ server/src/ServerInfo.ts
@@ -1,21 +1,30 @@
 const path = require("path");
 const fs = require("fs");
 
-export interface IServerInfo {
-    smtp : {
-    host: string, port: number,
-    auth: { user: string, pass: string }
-    },
-    imap : {
-    host: string, port: number,
-    auth: { user: string, pass: string }
-    } }
 
-// The configured server info.
+export interface IServerInfo {
+  smtp : {
+    host: string,
+    port: number,
+    auth: {
+      user: string,
+      pass: string
+    }
+  },
+  imap : {
+    host: string,
+    port: number,
+    auth: {
+      user: string,
+      pass: string
+    }
+  }
+}
+
+
 export let serverInfo: IServerInfo;
 
 
-// Read in the server information file.
 const rawInfo: string = fs.readFileSync(path.join(__dirname, "../serverInfo.json"));
 serverInfo = JSON.parse(rawInfo);
 console.log("ServerInfo: ", serverInfo);
